70db3b297f2627ed1814d6d439e6e5aa062be1e1
Christine <calvertc11@gmail.com>
2019-09-17 20:31:48 -0400

    Added full code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ISM4300Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] NumericalArray = new int[25];
+
+                for (int i = 0; i < 25; i++)
+                {
+                    NumericalArray[i] = i;
+                    Console.WriteLine("Element value = " + (i + 1));
+
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("There was an error with this program, please run it again");
+            }
+        }
+    }
+}
