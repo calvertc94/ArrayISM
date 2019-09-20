4bec1a9a0d7b8548c33748f0c09b837868edd730
Christine <calvertc11@gmail.com>
2019-09-19 20:06:45 -0400

    Comments

--- Program.cs
+++ Program.cs
@@ -8,11 +8,15 @@ namespace ISM4300Arrays
         {
             try
             {
+                //Initializes an array with 25 indexes
                 int[] NumericalArray = new int[25];
 
+                //Loops through the code 25 times
                 for (int i = 0; i < 25; i++)
                 {
+                    //Sets the index value of the array to the current number of times the loop has run
                     NumericalArray[i] = i;
+                    //Prints 1-25 without printing the first number as 0
                     Console.WriteLine("Element value = " + (i + 1));
 
                 }
